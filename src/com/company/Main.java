e27b359b38bcc90a46a1c13e5de92df4db597101
Thomas STOCKER <thomas.stocker92@gmail.com>
2020-11-12 11:17:59 +0100

    add exo2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,55 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Monster bMonster = createMonster("Pikachu", 0);
+        //Monster bMonster = createMonster("Pikachu", 0);
+        Army bArmy = createArmy();
 
-        displayMonster(bMonster);
+        //displayMonster(bMonster);
 
-        for (int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 50; i++) {
             Monster tmpMonster = createMonster("Pikachu " + i, i);
-            displayMonster(tmpMonster);
+            //displayMonster(tmpMonster);
+            addMonster(bArmy, tmpMonster);
+        }
+
+        displayArmy(bArmy);
+    }
+
+    public static void displayArmy(Army army) {
+        for (int i = 0; i < army.top; i++) {
+            displayMonster(army.monsters[i]);
         }
     }
 
+    public static void addMonster(Army army, Monster monster)
+    {
+        if (army.top >= army.monsters.length) {
+            Monster[] tmpMonsters = new Monster[army.top + 1];
+
+            for (int i = 0; i < army.monsters.length; i++) {
+                tmpMonsters[i] = army.monsters[i];
+            }
+
+            army.monsters = tmpMonsters;
+        }
+
+        army.monsters[army.top] = monster;
+        army.top++;
+    }
+
+    public static Army createArmy() {
+        Army army = new Army();
+
+        army.monsters = new Monster[4];
+        army.top = 0;
+
+        return army;
+    }
+
     public static void displayMonster(Monster pMonster) {
-        System.out.println("Monster<"+pMonster.name+">");
+        if (pMonster != null) {
+            System.out.println("Monster<"+pMonster.name+">");
+        }
     }
 
     public static Monster createMonster(String pName, int pHealth) {
